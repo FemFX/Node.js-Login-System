17f43166c6049c5ba84712abc577ef77f8f2611e
FemFX <ignedko34@gmail.com>
2019-08-05 16:37:49 +0300

    mongoose models

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
+
+const userSChema = new Schema({
+    username: {
+        type: String,
+        required: true, 
+        unique: true
+    },
+    email : {
+        type : String
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+userSChema.pre('save', function (next) {
+    var user = this 
+
+    if (!user.isModified('password')) {
+        return next()
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
+})
+
+userSChema.methods.comparePassword = function (clientPassword, cb) {
+    bcrypt.compare(clientPassword, this.password, (err, isMatch) => {
+        if (err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+module.exports = mongoose.model('User', userSChema)
